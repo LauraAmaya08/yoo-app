d16832bcb2c374fec11b7ccfdee863f13de24d99
LauraAmaya08 <laurasofiaamayacasado@gmail.com>
2025-02-13 09:23:55 -0500

    feat: :tada: Starting project, tailwind is configurated

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="bg-red-500 text-white p-4 text-center text-8xl">
+      ¡Si ves esto en rojo, Tailwind está funcionando!
+    </div>
+  );
+}
+
