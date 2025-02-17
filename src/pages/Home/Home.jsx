34fd63cc98642f0ba6cfb1399507842b0d9d85dc
LauraAmaya08 <laurasofiaamayacasado@gmail.com>
2025-02-17 17:32:18 -0500

    feat: :sparkles: adding followers and follows count

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,7 +9,7 @@ export const Home = () => {
         <div className='w-full min-h-screen'>
             <div className="w-full h-auto flex items-start justify-between lg:gap-x-32 md:gap-x-16 sm:gap-x-8 gap-x-4 relative">
                 {/*Navbar navegador */}
-                <div className="lg:w-[16%] md:w-[17%] h-[100vh] pt-10 px-3 sticky top-0 left-0 lg:block md:block sm:hidden">
+                <div className="lg:w-[16%] md:w-[17%] h-[100vh] pt-10 px-3 sticky top-0 left-0 lg:block md:block sm:hidden hidden">
                     <NavBarComputer/>
                 </div>
                 {/*Navbar movil*/}
--- src/pages/Profile/Profile/Profile.jsx
+++ src/pages/Profile/Profile/Profile.jsx
@@ -5,6 +5,8 @@ import axios from 'axios'
 
 const Profile = () => {
     const [user, setUser] = useState(null);
+    const [numSeguidores, setNumSeguidores] = useState(0);
+    const [numSeguidos, setNumSeguidos] = useState(0);
     
     useEffect(() => {
         axios.get("http://localhost:8080/api/profile", {
@@ -17,17 +19,44 @@ const Profile = () => {
             console.error("Error al obtener los datos del usuario:", error);
         });
     }, []);
+
+    useEffect(() => {
+    if(user){
+            axios.get(`http://localhost:8080/api/v1/seguimiento/${user.id}/contarSeguidores`, {
+                withCredentials: true
+            })
+            .then(response => {
+                setNumSeguidores(response.data);
+            })
+            .catch(error => {
+                console.error("Error al obtener los datos del usuario:", error);
+            });
+    }}, [user]);
+
+    useEffect(() => {
+        if(user){
+                axios.get(`http://localhost:8080/api/v1/seguimiento/${user.id}/contarSeguidos`, {
+                    withCredentials: true
+                })
+                .then(response => {
+                    setNumSeguidos(response.data);
+                })
+                .catch(error => {
+                    console.error("Error al obtener los datos del usuario:", error);
+                });
+        }}, [user]);
+    
     
     return (
         <>
-            <div className="lg:w-[88%] md:w-[88%] sm:w-full w-full h-auto lg:block md:block sm:hidden hidden">
+            <div className="lg:w-[80%] md:w-[80%] sm:w-full w-full h-auto lg:block md:block sm:hidden hidden">
                 <div className="w-full h-auto flex items-center lg:gap-x-20 md:gap-x-16 sm:gap-x-12 gap-x-8 justify-center mb-10">
                     {user ? (
                         <>
                             <img 
                                 src={user.fotoPerfil}
                                 alt="Perfil icono"
-                                className="rounded-full lg:w-44 md:w-44 sm:w-36 w-36 lg:h-44 md:h-44 sm:h-36 h-36 object-cover"
+                                className="rounded-full lg:w-44 md:w-44 sm:w-36 w-32 lg:h-44 md:h-44 sm:h-36 h-36 object-cover"
                             />
                             <div className="flex items-start flex-col">
                                 <div className="flex items-center gap-x-5 mb-4">
@@ -40,10 +69,10 @@ const Profile = () => {
                                 <div className="flex items-center gap-x-6 mb-4">
                                     <h6 className="text-base font-normal">10 Posts</h6>
                                     <Link to="/" className="text-base font-normal">
-                                        1200 Followers
+                                        {numSeguidores} Seguidores
                                     </Link>
                                     <Link to="/" className="text-base font-normal">
-                                        100 Following
+                                        {numSeguidos} Seguidos
                                     </Link>
                                 </div>
                                 <p className="text-base font-[Jost-Regular]">{user.nombreUser}</p>
@@ -52,12 +81,12 @@ const Profile = () => {
                                     {user.biografia} <br />
                                 </p>
                             </div>
+                            
                         </>
                     ) : (
                         <div className="w-8 h-8 rounded-full bg-gray-300 animate-pulse"></div>
                     )}
                 </div>
-
                 {user && (
                     <div className="w-full h-auto">
                         <div className="mt-4 transition-opacity duration-300 ease-out opacity-100">
